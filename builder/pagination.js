0565a610007d8b4416e211ce93d4102fc6b871d8
alexey <alexey@iprojects.ru>
2016-02-15 19:13:34 +0300

    Delete trash

--- builder/pagination.js
+++
@@ -1,7 +0,0 @@
-AppFilters.filter('sumstatPaginationFilter', function() {
-    _log('sumstatpagination arguments:',arguments);
-  return function(items, currentPage, itemsPerPage) {
-    return items.slice((currentPage - 1) * itemsPerPage, currentPage * itemsPerPage - 1);
-    // return input ? '\u2713' : '\u2718';
-  };
-});
